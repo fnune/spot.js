699fabb52ca2f0f31531af082db098684c6c7116
Fausto <fausto.nunez@outlook.com>
2016-04-30 23:19:40 +0200

    Fixed onresize functionality

--- spot.js
+++ spot.js
@@ -25,10 +25,10 @@ function getMousePosition(event) {
 /* DEFINE ELEMENT CONSTRUCTOR */
 function SpotInstance(elem) {
 	this.elem = elem;
-	this.elemX = window.scrollX + this.elem.getBoundingClientRect().left + (this.elem.offsetWidth / 2);
-	this.elemY = window.scrollY + this.elem.getBoundingClientRect().top + (this.elem.offsetHeight / 2);
+	const elemX = window.scrollX + elem.getBoundingClientRect().left + (elem.offsetWidth / 2);
+	const elemY = window.scrollY + elem.getBoundingClientRect().top + (elem.offsetHeight / 2);
 	this.getRadians = function() {
-		var rad = Math.atan2(this.elemY - mouseY, mouseX - this.elemX);
+		var rad = Math.atan2(elemY - mouseY, mouseX - elemX);
 		return rad;
 	}
 	this.getAngle = function() {
@@ -39,25 +39,28 @@ function SpotInstance(elem) {
 		return angle;
 	}
 	this.getDistance = function() {
-		var dist = Math.round(Math.sqrt(Math.pow(mouseX - this.elemX, 2) + Math.pow(this.elemY - mouseY, 2)));
+		var dist = Math.round(Math.sqrt(Math.pow(mouseX - elemX, 2) + Math.pow(elemY - mouseY, 2)));
 		return dist;
 	}
 }
 
 /* CREATE INSTANCES OF SELECTED ELEMENTS */
-for (i = 0; i < elems.spotIndicator.length; i += 1) {
-	spotIndicatorInstance[i] = new SpotInstance(elems.spotIndicator[i]);
-}
-for (i = 0; i < elems.spotShadow.length; i += 1) {
-	spotShadowInstance[i] = new SpotInstance(elems.spotShadow[i]);
-}
-for (i = 0; i < elems.spotRotate.length; i += 1) {
-	spotRotateInstance[i] = new SpotInstance(elems.spotRotate[i]);
-}
-for (i = 0; i < spotIndicatorInstance.length; i += 1) {
-    indicator[i] = document.createElement("div");
-    indicator[i].style.cssText = "display:table; position:absolute; pointer-events:none; left:" + (window.scrollX + spotIndicatorInstance[i].elem.getBoundingClientRect().left) + "px; top:" + (window.scrollY + spotIndicatorInstance[i].elem.getBoundingClientRect().top) + "px; height:" + spotIndicatorInstance[i].elem.clientHeight + "px; width:" + spotIndicatorInstance[i].elem.clientWidth + "px; color:white; text-align:center; font-weight:600; font-family:Arial; z-index:999999; outline:15px solid rgba(0,200,0,0.3); outline-offset:-15px; background:rgba(0,0,0,0.2);";
-    document.body.appendChild(indicator[i]);
+function createSpotInstances() {
+	for (i = 0; i < elems.spotIndicator.length; i += 1) {
+		spotIndicatorInstance[i] = new SpotInstance(elems.spotIndicator[i]);
+	}
+	for (i = 0; i < elems.spotShadow.length; i += 1) {
+		spotShadowInstance[i] = new SpotInstance(elems.spotShadow[i]);
+	}
+	for (i = 0; i < elems.spotRotate.length; i += 1) {
+		spotRotateInstance[i] = new SpotInstance(elems.spotRotate[i]);
+	}
+	for (i = 0; i < spotIndicatorInstance.length; i += 1) {
+	    indicator[i] = document.createElement("div");
+	    indicator[i].className = "spot-indicator-overlay";
+	    indicator[i].style.cssText = "display:table; position:absolute; pointer-events:none; left:" + (window.scrollX + spotIndicatorInstance[i].elem.getBoundingClientRect().left) + "px; top:" + (window.scrollY + spotIndicatorInstance[i].elem.getBoundingClientRect().top) + "px; height:" + spotIndicatorInstance[i].elem.clientHeight + "px; width:" + spotIndicatorInstance[i].elem.clientWidth + "px; color:white; text-align:center; font-weight:600; font-family:Arial; z-index:999999; outline:15px solid rgba(0,200,0,0.3); outline-offset:-15px; background:rgba(0,0,0,0.2);";
+	    document.body.appendChild(indicator[i]);
+	}
 }
 
 /* DEFINE AND APPLY THE DIFFERENT EFFECTS */
@@ -76,17 +79,35 @@ function initiateSpotEffects(event) {
 	}
 }
 
+/* CLEAR ALL EFFECTS SO THEY DON'T INTERFERE WITH NEW ONES */
+function clearAllEffects() {
+	for (i = 0; i < spotShadowInstance.length; i += 1) {
+        spotShadowInstance[i].elem.style.webkitFilter = "none";
+	}
+	for (i = 0; i < spotRotateInstance.length; i += 1) {
+		spotRotateInstance[i].elem.style.transform = "none";
+	}
+	var indicatorOverlays = document.getElementsByClassName("spot-indicator-overlay");
+	for (i = 0; i < indicatorOverlays.length; i += 1) {
+		indicatorOverlays[i].parentNode.removeChild(indicatorOverlays[i]);
+	}
+}
+
 /* EVENT LISTENERS */
 window.onmousemove = function(event) {
 	initiateSpotEffects(event);
 }
 
 window.onload = function() {
+	createSpotInstances();
 }
 
 window.onresize = function() {
+	clearAllEffects();
+	createSpotInstances();
 }
 
-window.onscroll = function() {
-	initiateSpotInstances();
-}
+/*
+window.onscroll = function(event) {
+	initiateSpotEffects(event);
+}*/
